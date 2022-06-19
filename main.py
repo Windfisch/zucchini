ebe3d1bac9b8257e78b02c639a01535e123e4d90
Florian Jung <flo@windfis.ch>
2022-06-19 03:37:11 +0200

    After all... why shouldn't I put this under the AGPL?

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
+# This program is free software: you can redistribute it and/or modify it under
+# the terms of the GNU Affero General Public License version 3 as published by
+# the Free Software Foundation at <https://www.gnu.org/licenses/agpl-3.0>.
+
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE.  See the GNU Affero General Public License for more
+# details.
+
 import network
 import re
 import gc
